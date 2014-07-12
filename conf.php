faa90c5f48c08ed88f723de9ba2d9e12ddaff911
Carlos Reyes <chentito002@gmail.com>
2014-07-12 10:32:10 -0500

    Create conf.php

---
+++ conf.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * Datos de conexion a la bd
+ */
+
+define("_DBUSER_","usuario_mysql");
+define("_DBPASSWORD_","password_mysql");
+define("_DBDATABASE_","nombre_basedatos");
+define("_DBHOST_","hots_basedatos");
